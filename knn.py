67f5504b0901573f727011b21f273af9d061f4fc
wzeng <willzeng3@gmail.com>
2023-01-22 23:53:42 -0600

    draft of classes

--- knn.py
+++ knn.py
@@ -2,7 +2,7 @@ import os
 from utils import external_ticks, constants
 from sklearn.model_selection import train_test_split
 # Machine learning libraries
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import pandas as pd
 import pandas_ta
@@ -16,7 +16,7 @@ from sklearn.model_selection import cross_val_score, KFold
 from sklearn import preprocessing
 import tech_indicators
 import argparse
-
+from models import BaseModel
 
 
 # Instantiate KNN learning model(k=15)
@@ -25,90 +25,97 @@ from hyperopt import hp, fmin, tpe, STATUS_OK, Trials, anneal
 
 n_neighbor_list = list(range(5, 100))
 leaf_size_list = list(range(1, 100))
-
-def create_parameter_space():
-    parameter_space = {'n_neighbors': hp.choice('n_neighbors', n_neighbor_list),
-                       'weights': hp.choice('weights', ['distance']),
-                       'algorithm': hp.choice('algorithm', ['brute', 'ball_tree', 'kd_tree']),
-                       'leaf_size': hp.choice('leaf_size', leaf_size_list),
-                       'p': hp.choice('p', [1, 2]),
-                       'metric': hp.choice('metric', ['minkowski', 'chebyshev']),
-                       }
-    return parameter_space
+class KNN(BaseModel):
+    def __init__(self, options, data_frame):
+        super().__init__(options, data_frame)
+        self.model = KNeighborsClassifier
+        self.model_name = 'knn'
 
 
-best = math.inf
+
+    def create_parameter_space():
+        parameter_space = {'n_neighbors': hp.choice('n_neighbors', n_neighbor_list),
+                           'weights': hp.choice('weights', ['distance']),
+                           'algorithm': hp.choice('algorithm', ['brute', 'ball_tree', 'kd_tree']),
+                           'leaf_size': hp.choice('leaf_size', leaf_size_list),
+                           'p': hp.choice('p', [1, 2]),
+                           'metric': hp.choice('metric', ['minkowski', 'chebyshev']),
+                           }
+        return parameter_space
 
 
-def f(params):
-    global best
-    acc = accuracy_model(params)
-    if acc < best:
-        best = acc
-    return {'loss': acc, 'status': STATUS_OK}
-
-def accuracy_model(params):
-    knn = KNeighborsRegressor(**params)
-    knn.fit(X_train, Y_train['Close'])
-    y_pred = knn.predict(X_test)
-    return mean_squared_error(Y_test['Close'], y_pred, squared=False)
-
-# Finding out which set of hyperparameters give highest accuracy
-def find_best_parameters(parameter_space):
-    trials = Trials()
-    best_parameters = fmin(fn=f,
-                       space=parameter_space,
-                       algo=tpe.suggest,  # the logic which chooses next parameter to try
-                       max_evals=100,
-                       trials=trials
-                       )
-    algo_map = {0: 'brute',
-                1: 'ball_tree',
-                2: 'kd_tree'}
-    weight_map = {0: 'distance'}
-    metric_map = {0: 'minkowski',
-                  1: 'chebyshev'}
-    for k, v in best_parameters.items():
-        if k == 'algorithm':
-            best_parameters[k] = algo_map[v]
-        elif k == 'weights':
-            best_parameters[k] = weight_map[v]
-        elif k == 'metric':
-            best_parameters[k] = metric_map[v]
-        elif k == 'p':
-            best_parameters[k] = v+1
-        elif k == 'leaf_size':
-            best_parameters[k] = leaf_size_list[v]
-        elif k == 'n_neighbors':
-            best_parameters[k] = n_neighbor_list[v]
-    print('best: ', best, 'with best params :', best_parameters)
-    return best_parameters
+    best = math.inf
 
 
-def train_and_predict(xtrain, xtest, ytrain, ytest, parameters):
-    knn = KNeighborsRegressor(**parameters)
-    knn.fit(xtrain, ytrain['Close'])
-    y_pred = knn.predict(xtest)
-    rmse = mean_squared_error(ytest['Close'], y_pred, squared=False)
-    print("knn rmse: ", rmse)
-    return y_pred, rmse
-#
-def generate_plots(y_predicted, Y_test, rmse, name, length_of_moving_averages=10):
-    df2 = pd.DataFrame(data=y_predicted, index=Y_test.index, columns=['predicted']).astype('float')
-    df_temp = pd.concat((pd.DataFrame(data=y_predicted, index=Y_test.index), Y_test['Close']), keys=['predicted', 'close'], axis=1)
-    # print(df_temp.head)
-    df3 = Y_test[['Close']].astype('float').rename(columns={'Close':'actual'})
-    ax = df2.plot()
-    df3.plot(ax=ax, title='pred values vs real values', fontsize=10)
+    def f(params):
+        global best
+        acc = accuracy_model(params)
+        if acc < best:
+            best = acc
+        return {'loss': acc, 'status': STATUS_OK}
 
-    ax.set_xlabel('Date')
-    ax.set_ylabel('{} Price'.format(name))
-    plt.text(0.5, 0.5, 'rmse: '+str(rmse), ha='center', va='center', fontsize='small')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plot_name = "{}_knn_plot.png".format(length_of_moving_averages)
-    plt.savefig(plot_name, dpi=500)
-    print(plot_name)
+    def accuracy_model(params):
+        knn = KNeighborsClassifier(**params)
+        knn.fit(X_train, Y_train['Close'])
+        y_pred = knn.predict(X_test)
+        return mean_squared_error(Y_test['Close'], y_pred, squared=False)
+
+    # Finding out which set of hyperparameters give highest accuracy
+    def find_best_parameters(parameter_space):
+        trials = Trials()
+        best_parameters = fmin(fn=f,
+                           space=parameter_space,
+                           algo=tpe.suggest,  # the logic which chooses next parameter to try
+                           max_evals=100,
+                           trials=trials
+                           )
+        algo_map = {0: 'brute',
+                    1: 'ball_tree',
+                    2: 'kd_tree'}
+        weight_map = {0: 'distance'}
+        metric_map = {0: 'minkowski',
+                      1: 'chebyshev'}
+        for k, v in best_parameters.items():
+            if k == 'algorithm':
+                best_parameters[k] = algo_map[v]
+            elif k == 'weights':
+                best_parameters[k] = weight_map[v]
+            elif k == 'metric':
+                best_parameters[k] = metric_map[v]
+            elif k == 'p':
+                best_parameters[k] = v+1
+            elif k == 'leaf_size':
+                best_parameters[k] = leaf_size_list[v]
+            elif k == 'n_neighbors':
+                best_parameters[k] = n_neighbor_list[v]
+        print('best: ', best, 'with best params :', best_parameters)
+        return best_parameters
+
+
+    def train_and_predict(xtrain, xtest, ytrain, ytest, parameters):
+        knn = KNeighborsRegressor(**parameters)
+        knn.fit(xtrain, ytrain['Close'])
+        y_pred = knn.predict(xtest)
+        rmse = mean_squared_error(ytest['Close'], y_pred, squared=False)
+        print("knn rmse: ", rmse)
+        return y_pred, rmse
+    #
+    def generate_plots(y_predicted, Y_test, rmse, name, length_of_moving_averages=10):
+        df2 = pd.DataFrame(data=y_predicted, index=Y_test.index, columns=['predicted']).astype('float')
+        df_temp = pd.concat((pd.DataFrame(data=y_predicted, index=Y_test.index), Y_test['Close']), keys=['predicted', 'close'], axis=1)
+        # print(df_temp.head)
+        df3 = Y_test[['Close']].astype('float').rename(columns={'Close':'actual'})
+        ax = df2.plot()
+        df3.plot(ax=ax, title='pred values vs real values', fontsize=10)
+
+        ax.set_xlabel('Date')
+        ax.set_ylabel('{} Price'.format(name))
+        plt.text(0.5, 0.5, 'rmse: '+str(rmse), ha='center', va='center', fontsize='small')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plot_name = "{}_knn_plot.png".format(length_of_moving_averages)
+        plt.savefig(plot_name, dpi=500)
+        print(plot_name)
 
 def build_args():
     parser = argparse.ArgumentParser()
