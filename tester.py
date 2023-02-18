2cb223024d76caf4eb650321d4a3cba541a1f945
wzeng <willzeng3@gmail.com>
2023-02-17 22:46:05 -0600

    zsdf

--- tester.py
+++ tester.py
@@ -2,27 +2,49 @@ import unittest
 import os
 from utils import constants
 import pandas as pd
-from analyzer import compute_portvals, graph_order_book
+from analyzer import compute_portvals, graph_order_book, compute_yearly_gains
 import tech_indicators
-
+from datetime import datetime
 
 class TestMarketSimETH(unittest.TestCase):
     def setUp(self):
         file_name = os.path.join(constants.YAHOO_DATA_DIR, 'real_eth.csv')
         self.data_frame_from_file = tech_indicators.read_df_from_file(file_name)
-        self.order_bk = pd.read_csv('tester1.csv', index_col=[0], header=[0], skipinitialspace=True)
+        self.long_short_order_bk = pd.read_csv('tester1.csv', index_col=[0], header=[0], skipinitialspace=True)
 
-    def test_create_correct_order_book(self):
-        order_book = compute_portvals(self.order_bk, self.data_frame_from_file[['Close']])
-        sum_series = order_book.sum()
+    def test_create_long_short_portfolio(self):
+        portfolio = compute_portvals(self.long_short_order_bk, self.data_frame_from_file[['Close']], 0)
+        sum_series = portfolio.sum()
         self.assertEqual(sum_series['share_amount'], 18900.0)
         self.assertEqual(sum_series['bs_signal'], 1)
         self.assertEqual(sum_series['total_liquid_cash'], 938395.1612033314)
         self.assertEqual(sum_series['holding'], 100.0)
         self.assertEqual(sum_series['close'], 115489.94024682)
+        import pdb
+        pdb.set_trace()
+
+    def test_compute_yearly_gains(self):
+        portfolio = compute_portvals(self.long_short_order_bk, self.data_frame_from_file[['Close']], 0)
+        yearly_gains_dict = compute_yearly_gains(portfolio)
+        self.assertEqual(yearly_gains_dict[1], 564.3000105961332)
+        self.assertEqual(yearly_gains_dict[2], -100.08058630201029)
+        self.assertEqual(yearly_gains_dict[3], 136.26685212517867)
+        self.assertEqual(yearly_gains_dict[4], 316.76851649782077)
+        self.assertEqual(yearly_gains_dict[5], 154.6028791479902)
+
+        first = portfolio.loc['2017-12-10':'2018-12-10']['total_liquid_cash'].sum()
+        second = portfolio.loc['2018-12-10':'2019-12-10']['total_liquid_cash'].sum()
+        third = portfolio.loc['2019-12-10':'2020-12-10']['total_liquid_cash'].sum()
+        fourth = portfolio.loc['2020-12-10':'2021-12-10']['total_liquid_cash'].sum()
+        fifth = portfolio.loc['2021-12-10':'2022-12-10']['total_liquid_cash'].sum()
+
+        total_percent_gain = ((portfolio['total_liquid_cash'].sum() - portfolio['total_liquid_cash'][0])/abs(portfolio['total_liquid_cash'][0])) * 100
+
+        import pdb
+        pdb.set_trace()
+
 
     def test_create_ylabels(self):
         df_with_ytest = tech_indicators.create_ylabels(self.data_frame_from_file, lookahead_days=5)
-        import pdb
-        pdb.set_trace()
+
 
